e1bf561d530b3307c2a466d7c01a4acd649b3b45
Nagaraj <root@adminuser-VirtualBox.(none)>
2015-10-18 17:35:56 +0300

    nginx cookbook

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,17 @@
+execute "apt-get update" do
+command "apt-get update"
+end
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/www/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
